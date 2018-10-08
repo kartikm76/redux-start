be940e6d511edd6eb1b5306c53cf7ad14c82bde8
kartikm76 <kartikm76@yahoo.com>
2018-10-08 12:16:20 +0530

    Redux Basic Example

--- src/redux-basics.js
+++ src/redux-basics.js
@@ -1,20 +1,38 @@
 const redux = require('redux');
 const createStore = redux.createStore;
+
+const initialState = {
+    counter: 0
+}
+
 // Reducer
-const rootReducer = (state, action) => {
+const rootReducer = (state = initialState, action) => {
+    if (action.type === 'INC_COUNTER') {
+        return {
+            ...state,
+            counter: state.counter + 1
+        };
+    }
+    if (action.type === 'ADD_COUNTER') {
+        return {
+            ...state,
+            counter: state.counter + action.value
+        };
+    }
     return state; /// just returning old state
 };
 
 // Store
 const store = createStore(rootReducer);
-console.log(store.getState());
-
-
-
-
-
-// Action
-
-// Dispatching Action
+console.log("Store : ", store.getState());
 
 // Subscription
+store.subscribe(() => {
+    console.log('[Subscription]', store.getState());
+}); // arg is a function that is executed whenever store is updated
+
+// Dispatching Action
+store.dispatch({type: 'INC_COUNTER'});
+store.dispatch({type: 'ADD_COUNTER', value: 10});
+console.log("After dispatched and subscribed: ", store.getState());
+
